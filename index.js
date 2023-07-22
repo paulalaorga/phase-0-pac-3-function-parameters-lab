0538de2dd13c47a3bd630b27dbf5828dca9713eb
paula <paulalaorga@gmail.com>
2023-07-22 10:54:51 +1000

    index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+function introduction(name) {
+  return `Hi, my name is ${name}.`;
+}
+
+function introductionWithLanguage(Name, Language) {
+    return `Hi, my name is ${Name} and I am learning to program in ${Language}.`;
+}
+
+function introductionWithLanguageOptional(Name, Language = "JavaScript") {
+  return `Hi, my name is ${Name} and I am learning to program in ${Language}.`;
+}
+
+function introductionWithLanguageOptional(Name, Language = "JavaScript") {
+  return `Hi, my name is ${Name} and I am learning to program in ${Language}.`;
+}
